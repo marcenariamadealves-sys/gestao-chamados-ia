45e0728c6e677ac08f40517fc7f94bc783b82edd
marcenariamadealves-sys <marcenariamadealves@gmail.com>
2025-11-06 22:57:19 -0300

    Create HomeController.cs
    Criar controlador principal HomeController.cs

---
+++ web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoChamadosWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+    }
+}
