8cb7d23e1884fac1916187a65929ccc7a6713d12
marcenariamadealves-sys <marcenariamadealves@gmail.com>
2025-11-06 23:08:45 -0300

    Update ChamadosController.cs
    Atualizando ChamadosController para usar ChamadoService

--- backend/Controllers/ChamadosController.cs
+++ backend/Controllers/ChamadosController.cs
@@ -1,22 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
+using GestaoChamadosAPI.Modelos;
+using GestaoChamadosAPI.Servicos;
+using System.Collections.Generic;
 
-namespace GestaoChamadosAPI.Controllers
+namespace GestaoChamadosAPI.Controladores
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ChamadosController : ControllerBase
     {
+        private readonly ChamadoService _service = new ChamadoService();
+
         [HttpGet]
-        public IActionResult ListarChamados()
+        public ActionResult<List<Chamado>> GetTodos()
         {
-            return Ok(new[] { "Chamado 1", "Chamado 2" });
+            return Ok(_service.ListarTodos());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Chamado> GetPorId(int id)
+        {
+            var chamado = _service.ObterPorId(id);
+            if (chamado == null) return NotFound();
+            return Ok(chamado);
         }
 
         [HttpPost]
-        public IActionResult CriarChamado([FromBody] object chamado)
+        public ActionResult Adicionar([FromBody] Chamado chamado)
         {
-            return Ok("Chamado criado com sucesso!");
+            _service.Adicionar(chamado);
+            return CreatedAtAction(nameof(GetPorId), new { id = chamado.Id }, chamado);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Atualizar(int id, [FromBody] Chamado chamado)
+        {
+            chamado.Id = id;
+            _service.Atualizar(chamado);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Remover(int id)
+        {
+            _service.Remover(id);
+            return NoContent();
         }
     }
 }
-
